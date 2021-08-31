5b0d00f69a3e23a1b19edfe2aa7984aad62457bb
revanthkumaar <iamrevanthkumar@gmail.com>
2021-08-31 19:55:20 +0530

    aldkj;l
    kqjw;jasd;f

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const landRecBackend = express()
 
+const Blockchain = require("./blockchain");
+const landRec = new Blockchain();
+
+
 landRecBackend.get('/home',function(req,res){
     res.send('this is home page');
 })
 
 landRecBackend.get("/blockchain", function (req, res) {
-  res.send("blockchain end point");
+  res.send(landRec);
+});
+
+landRecBackend.post('/transaction',function(req,res){
+
+   const sellerName = req.body.seller;
+   const receiverName = req.body.receiver;
+   const assetValue = req.body.assetValue;
+
+   const transaction = {
+       sellerName: sellerName,
+       receiverName: receiverName,
+       assetValue: assetValue
+   }
+
+   console.log(transaction);
+
+
 });
 
 landRecBackend.listen(3000)
