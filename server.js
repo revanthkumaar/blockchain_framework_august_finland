a16cd7c35c53ee81bb4bf9c874526a1cf748e203
revanthkumaar <iamrevanthkumar@gmail.com>
2021-09-01 18:40:02 +0530

    ewdfjsd
    a;dflkajs;

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const landRecBackend = express()
-
+const bodyParser = require('body-parser');
 const Blockchain = require("./blockchain");
 const landRec = new Blockchain();
+landRecBackend.use(bodyParser.json());
+landRecBackend.use(bodyParser.urlencoded({ extended: false }));
 
 
 landRecBackend.get('/home',function(req,res){
@@ -18,16 +20,18 @@ landRecBackend.post('/transaction',function(req,res){
    const sellerName = req.body.seller;
    const receiverName = req.body.receiver;
    const assetValue = req.body.assetValue;
-
    const transaction = {
        sellerName: sellerName,
        receiverName: receiverName,
        assetValue: assetValue
    }
-
    console.log(transaction);
-
+   res.json({"message":"transaction is created"})
 
 });
 
-landRecBackend.listen(3000)
+
+
+landRecBackend.listen(3000, function() {
+	console.log(`Listening on port 3000`);
+});
