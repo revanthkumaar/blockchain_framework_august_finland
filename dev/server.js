48fe187b0790ce9104cad0dbad230e6f32e32f15
revanthkumaar <iamrevanthkumar@gmail.com>
2021-09-03 19:13:52 +0530

    checkin
    checkin

--- dev/server.js
+++ dev/server.js
@@ -5,6 +5,7 @@ const Blockchain = require("./blockchain");
 const landRec = new Blockchain();
 landRecBackend.use(bodyParser.json());
 landRecBackend.use(bodyParser.urlencoded({ extended: false }));
+const rp = require('request-promise')
 
 const port = process.argv[2];
 
@@ -28,6 +29,43 @@ landRecBackend.post("/transaction", function (req, res) {
   console.log(transaction);
   res.json({ message: "transaction is created" });
 });
+
+landRecBackend.post('register-broadcast-node',function(req,res){
+  //STEP-1 register the new node address at the node where it pings first
+  const newNodeUrl = req.body.newNodeurl;
+  if(landRec.networkNodes.indexOf(newNodeUrl) == -1) {
+    landRec.networkNodes.push(newNodeUrl);
+  }
+
+  //STEP-2 let 3002 broadcast the new node info to all the others in the network
+    const regNodesPromises = [];
+    landRec.networkNodes.forEach(networkNodeUrl => {
+      const requestOptions = {
+        url: networkNodeUrl + '/register-node',
+        method:'POST',
+        body:{newNodeUrl: newNodeUrl},
+        json: true
+      }
+      regNodesPromises.push(rp(requestOptions)); //call gets triggered
+
+    });
+
+    //STEP-3 BULK REGISTRY OF OTHER NODES AT 3004
+
+    Promise.all(regNodesPromises)
+    .then(data => {
+        const bulkRegisterOptions = {
+          url: newNodeUrl + '/register-nodes-bulk',
+          method:'POST',
+          body: {allNetworkNodes: [...landRec.networkNodes,landRec.currentNodeUrl]},
+          json: true
+        };
+        return rp(bulkRegisterOptions);
+    })
+    .then(data => {
+      res.json({note: 'new node registered successfully'})
+    });
+})
 
 landRecBackend.post("/register-node", function (req, res) {
   const newNodeAddress = req.body.nodeAddress;
