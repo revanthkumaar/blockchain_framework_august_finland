38fb1f7d0b569cb95352b2c9149fd428b47be02c
revanthkumaar <iamrevanthkumar@gmail.com>
2021-09-04 19:54:11 +0530

    sd;lfj;
    q;elksdfa

--- dev/server.js
+++ dev/server.js
@@ -5,7 +5,7 @@ const landRec = new Blockchain();
 const bodyParser = require("body-parser");
 landRecBackend.use(bodyParser.json());
 landRecBackend.use(bodyParser.urlencoded({ extended: false }));
-const rp = require('request-promise')
+const rp = require("request-promise");
 const port = process.argv[2];
 
 landRecBackend.get("/home", function (req, res) {
@@ -31,108 +31,146 @@ landRecBackend.post("/transaction", function (req, res) {
 
 //MAIN STEP-1 BROADCAST TRANSACTIONS //////////////////////////////////
 
-landRecBackend.post('/transaction/broadcast',function(req,res){
-   const newTransaction = landRec.createNewTransaction(req.body.seller,req.body.receiver,req.body.asset)
+landRecBackend.post("/transaction/broadcast", function (req, res) {
+  const newTransaction = landRec.createNewTransaction(
+    req.body.seller,
+    req.body.receiver,
+    req.body.asset
+  );
 
-   //broadcasting the transaction object to all other nodes
+  //broadcasting the transaction object to all other nodes
 
-   const requestPromises = [];
-   landRec.networkNodes.forEach((networkNodeUrl) => {
-     const requestOptions = {
-       url: networkNodeUrl + "/transaction",
-       method: "POST",
-       body: newTransaction,
-       json: true,
-     };
-     requestPromises.push(rp(requestOptions)); //call gets triggered
-   });
+  const requestPromises = [];
+  landRec.networkNodes.forEach((networkNodeUrl) => {
+    const requestOptions = {
+      url: networkNodeUrl + "/transaction",
+      method: "POST",
+      body: newTransaction,
+      json: true,
+    };
+    requestPromises.push(rp(requestOptions)); //call gets triggered
+  });
 
-   Promise.all(reqestPromises).then(data => {
-     res.json({note: 'transaction got successfully broadcasted, will take few mins to validate your transaction'})
-   })
-})
-
-//MAIN STEP-2 MINING THE BLOCKS
-landRecBackend.get('/mine',function(req,res){
-  //part-1
-  const lastBlock = landRec.getLastBlock()
-  const previousBlockHash = lastBlock['hash']
-  const currentBlockData = {
-    transactions: landRec.pendingTransactions
-  }
-  const nonce = landRec.proofOfWork(previousBlockHash,currentBlockData)
-  const blockHash = landRec.generateHash(previousBlockHash,currentBlockData, nonce)
- const newBlock = landRec.createNewBlock(nonce, previousBlockHash, hash)
-
-//part-2
-
-
- 
-
-})
-
-landRecBackend.post("/receive-new-block", function (req, res) {
-   const newBlock = req.body.newBlock;
-   const lastBlock = landRec.getLastBlock();
-   const correctHash = lastBlock.hash === newBlock.previousBlockHash;
-   const correctIndex = lastBlock["index"] + 1 === newBlock["index"];
-
-   if (correctHash && correctIndex) {
-     landRec.chain.push(newBlock);
-     landRec.pendingTransactions = [];
-     res.json({
-       note: "New block received and accepted.",
-       newBlock: newBlock,
-     });
-   } else {
-     res.json({
-       note: "New block rejected.",
-       newBlock: newBlock,
-     });
-   }
+  Promise.all(reqestPromises).then((data) => {
+    res.json({
+      note: "transaction got successfully broadcasted, will take few mins to validate your transaction",
+    });
+  });
 });
 
+//MAIN STEP-2 MINING THE BLOCKS
+landRecBackend.get("/mine", function (req, res) {
+  //part-1
+  const lastBlock = landRec.getLastBlock();
+  const previousBlockHash = lastBlock["hash"];
+  const currentBlockData = {
+    transactions: landRec.pendingTransactions,
+  };
+  const nonce = landRec.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = landRec.generateHash(
+    previousBlockHash,
+    currentBlockData,
+    nonce
+  );
+  const newBlock = landRec.createNewBlock(nonce, previousBlockHash, hash);
 
-landRecBackend.post('/register-broadcast-node',function(req,res){
+  //part-2 - broadcast node to all other nodes
+
+  const requestPromises = [];
+  landRec.networkNodes.forEach((networkNodeUrl) => {
+    const requestOptions = {
+      uri: networkNodeUrl + "/receive-new-block",
+      method: "POST",
+      body: { newBlock: newBlock },
+      json: true,
+    };
+
+    requestPromises.push(rp(requestOptions));
+  });
+
+  Promise.all(requestPromises)
+    .then((data) => {
+      const requestOptions = {
+        uri: landRec.currentNodeUrl + "/transaction/broadcast/reward",
+        method: "POST",
+        body: {
+          amount: 6.5,
+          sender: "00",
+          recipient: nodeAddress,
+        },
+        json: true,
+      };
+
+      return rp(requestOptions);
+    })
+    .then((data) => {
+      res.json({
+        note: "New block mined & broadcast successfully",
+        block: newBlock,
+      });
+    });
+});
+
+landRecBackend.post("/receive-new-block", function (req, res) {
+  const newBlock = req.body.newBlock;
+  const lastBlock = landRec.getLastBlock();
+  const correctHash = lastBlock.hash === newBlock.previousBlockHash;
+  const correctIndex = lastBlock["index"] + 1 === newBlock["index"];
+
+  if (correctHash && correctIndex) {
+    landRec.chain.push(newBlock);
+    landRec.pendingTransactions = [];
+    res.json({
+      note: "New block received and accepted.",
+      newBlock: newBlock,
+    });
+  } else {
+    res.json({
+      note: "New block rejected.",
+      newBlock: newBlock,
+    });
+  }
+});
+
+landRecBackend.post("/register-broadcast-node", function (req, res) {
   //STEP-1 register the new node address at the node where it pings first
   const newNodeUrl = req.body.newNodeUrl;
   console.log(newNodeUrl);
   console.log(landRec.networkNodes);
-  if(landRec.networkNodes.indexOf(newNodeUrl) == -1) {
+  if (landRec.networkNodes.indexOf(newNodeUrl) == -1) {
     landRec.networkNodes.push(newNodeUrl);
   }
 
   //STEP-2 let 3002 broadcast the new node info to all the others in the network
-    const regNodesPromises = [];
-    landRec.networkNodes.forEach(networkNodeUrl => {
-      const requestOptions = {
-        url: networkNodeUrl + '/register-node',
-        method:'POST',
-        body:{newNodeUrl: newNodeUrl},
-        json: true
-      }
-      regNodesPromises.push(rp(requestOptions)); //call gets triggered
+  const regNodesPromises = [];
+  landRec.networkNodes.forEach((networkNodeUrl) => {
+    const requestOptions = {
+      url: networkNodeUrl + "/register-node",
+      method: "POST",
+      body: { newNodeUrl: newNodeUrl },
+      json: true,
+    };
+    regNodesPromises.push(rp(requestOptions)); //call gets triggered
+  });
 
-    });
+  //STEP-3 BULK REGISTRY OF OTHER NODES AT 3004
 
-    //STEP-3 BULK REGISTRY OF OTHER NODES AT 3004
-
-    Promise.all(regNodesPromises)
-    .then(data => {
-        const bulkRegisterOptions = {
-          url: newNodeUrl + "/register-nodes-bulk",
-          method: "POST",
-          body: {
-            nodeAddresses: [...landRec.networkNodes, landRec.currentNodeUrl],
-          },
-          json: true,
-        };
-        return rp(bulkRegisterOptions);
+  Promise.all(regNodesPromises)
+    .then((data) => {
+      const bulkRegisterOptions = {
+        url: newNodeUrl + "/register-nodes-bulk",
+        method: "POST",
+        body: {
+          nodeAddresses: [...landRec.networkNodes, landRec.currentNodeUrl],
+        },
+        json: true,
+      };
+      return rp(bulkRegisterOptions);
     })
-    .then(data => {
-      res.json({note: 'new node registered successfully'})
+    .then((data) => {
+      res.json({ note: "new node registered successfully" });
     });
-})
+});
 
 landRecBackend.post("/register-node", function (req, res) {
   const newNodeAddress = req.body.nodeAddress;
