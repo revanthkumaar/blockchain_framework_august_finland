21b41f68cc106d0da9193a34bb25e0da26bec77e
revanthkumaar <iamrevanthkumar@gmail.com>
2021-09-06 19:33:33 +0530

    lakjsdlkj

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,64 +1,80 @@
-const sha256 = require('sha256')
+const sha256 = require("sha256");
 
-function Blockchain(){
-    this.chain = []; //to store the blocks information
-    this.pendingTransactions = [];
-    this.networkNodes = [];
-    this.currentNodeUrl = process.argv[3];
-    this.createNewBlock('0','0','0'); //genesis
+function Blockchain() {
+  this.chain = []; //to store the blocks information
+  this.pendingTransactions = [];
+  this.networkNodes = [];
+  this.currentNodeUrl = process.argv[3];
+  this.createNewBlock("0", "0", "0"); //genesis
 }
 
 //to create the block
-Blockchain.prototype.createNewBlock = function(nonce,previousBlockHash,hash){
-    const newBlock = {
-        index: this.chain.length+1,
-        timestamp:Date.now(),
-        transactions:this.pendingTransactions,
-        nonce:nonce,
-        hash:hash,
-        previousBlockHash:previousBlockHash
-    };
-    this.pendingTransactions = [];//once the block gets created delete all the pending trs
-    this.chain.push(newBlock);
-    return newBlock;
-}
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.pendingTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
+  this.pendingTransactions = []; //once the block gets created delete all the pending trs
+  this.chain.push(newBlock);
+  return newBlock;
+};
 
-Blockchain.prototype.getLastBlock = function(){
-   return this.chain[this.chain.length-1];
-}
+Blockchain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
 
 //to create a transaction
-Blockchain.prototype.createNewTransaction = function(amount,sender,recipient){
-    const newTransaction = {
-        amount: amount,
-        sender: sender,
-        recipient: recipient
-    };
-}
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient,
+  };
+};
 //to add trs to pending transactions
-Blockchain.prototype.addTransactionToPendingTransactions = function(transactionObj){
+Blockchain.prototype.addTransactionToPendingTransactions = function (
+  transactionObj
+) {
   this.pendingTransactions.push(transactionObj);
-}
+};
 
-Blockchain.prototype.generateHash = function(previousBlockHash,currentBlockData,nonce){
-  const dataString = previousBlockHash + JSON.stringify(currentBlockData) + nonce.toString();
+Blockchain.prototype.generateHash = function (
+  previousBlockHash,
+  currentBlockData,
+  nonce
+) {
+  const dataString =
+    previousBlockHash + JSON.stringify(currentBlockData) + nonce.toString();
   const hash = sha256(dataString);
-    return hash;
-}
+  return hash;
+};
 
-Blockchain.prototype.proofOfWork = function(previousBlockHash,currentBlockData){
-    let nonce = 0;
-    let hash = this.generateHash(previousBlockHash,currentBlockData,nonce)
-    //cryptographic puzzle
-    while(hash.substring(0,5) != '00000'){
-        nonce++;
-        hash = this.generateHash(previousBlockHash, currentBlockData, nonce);
-        console.log(hash);
-    }
-    return nonce;
- 
-}
-
+Blockchain.prototype.proofOfWork = function (
+  previousBlockHash,
+  currentBlockData
+) {
+  let nonce = 0;
+  let hash = this.generateHash(previousBlockHash, currentBlockData, nonce);
+  //cryptographic puzzle
+  while (hash.substring(0, 5) != "00000") {
+    nonce++;
+    hash = this.generateHash(previousBlockHash, currentBlockData, nonce);
+    console.log(hash);
+  }
+  return nonce;
+};
 
 Blockchain.prototype.chainIsValid = function (blockchain) {
   let validChain = true;
@@ -95,6 +111,5 @@ Blockchain.prototype.chainIsValid = function (blockchain) {
 
   return validChain;
 };
-
 
 module.exports = Blockchain; //give access to other files to import the constructor Blockchain
